d3130166412282381f3f29b9cfdc6185afedf893
DaHermann <ndanielhermann@gmail.com>
2025-05-29 20:57:09 +0000

    final implementattion

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -33,14 +33,12 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-      dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
 
   };
 
   const handleRemove = (item) => {
-      dispatch(removeItem(item));
-      // dispatch(updateQuantity({ name: item.name, quantity: item.quantity}))
-
+      dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
@@ -74,7 +72,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e)=>handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -10,7 +10,7 @@ function ProductList({ onHomeClick }) {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
-    // const cart = useSelector(state => state.cart);
+    const CartItems = useSelector(state => state.cart.items);
 
 
     const plantsArray = [
@@ -270,6 +270,16 @@ function ProductList({ onHomeClick }) {
         }));
     };
 
+    const calculateTotalQuantity = () => {
+        return CartItems ? CartItems.reduce((total, item) => total + item.quantity, 0) : 0;
+    };
+
+    const handleDesable = (plantName) => {
+        const alradyAdded = CartItems.find(item => item.name == plantName)
+
+        console.log (plantName+' : ', alradyAdded )
+        return  alradyAdded ? true : false;
+    }
 
 
     return (
@@ -289,7 +299,12 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div>
+                        <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                        <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1>
+                            <span className="cartItemNumber">{calculateTotalQuantity()}</span>
+                        </a>
+                    </div>
                 </div>
             </div>
             {!showCart ? (
@@ -314,6 +329,7 @@ function ProductList({ onHomeClick }) {
                                         <button
                                             className="product-button"
                                             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
+                                            disabled={handleDesable(plant.name)}
                                         >
                                             Add to Cart
                                         </button>
